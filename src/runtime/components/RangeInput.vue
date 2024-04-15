3815860e77c3b1a7ba990c56b201bccd170bde07
Matteo Rigoni <matteo.rigoni@atoms.studio>
2024-04-15 20:31:04 +0200

    fix: precision property

--- src/runtime/components/RangeInput.vue
+++ src/runtime/components/RangeInput.vue
@@ -1,60 +1,52 @@
 <script lang="ts" setup>
-  import { useAisWidget } from "../composables/useAisWidget";
-  import { useAisRangeInputRenderState } from "../composables/useAisRangeInput";
-  import { useSuit } from "../composables/useSuit";
-  import { ref, computed, type Ref } from "vue";
-import type { range } from "instantsearch.js/es/lib/utils";
+import { useAisWidget } from "../composables/useAisWidget";
+import { useAisRangeInputRenderState } from "../composables/useAisRangeInput";
+import { useSuit } from "../composables/useSuit";
+import { ref, computed, type Ref } from "vue";
 
-  const props = defineProps({
-    attribute: {
-        type: String,
-        required: true,
-    },
-  })
+const props = withDefaults(
+  defineProps<{
+    attribute: string;
+    precision?: number;
+  }>(),
+  { precision: 1 },
+);
 
-  const { state: rangesState } = useAisWidget("range");
-  const rangeState = useAisRangeInputRenderState();
-  const suit = useSuit("RangeInput");
+const { state: rangesState } = useAisWidget("range");
+const rangeState = useAisRangeInputRenderState();
+const suit = useSuit("RangeInput");
 
-  const state = computed(() => {
-    return rangeState.value[props.attribute]
-      ? rangeState.value[props.attribute]
-      : rangesState.value[props.attribute];
-  });
+const state = computed(() => {
+  return rangeState.value[props.attribute]
+    ? rangeState.value[props.attribute]
+    : rangesState.value[props.attribute];
+});
 
-  const minInput: Ref<number> = ref()
-  const maxInput: Ref<number> = ref()
+const minInput: Ref<number> = ref();
+const maxInput: Ref<number> = ref();
 
-  const widgetParams = computed(
-    () => rangesState.value[props.attribute].widgetParams,
-  );
+const step = computed(() => 1 / Math.pow(10, props.precision));
 
-  const step = computed(() => 1 / Math.pow(10, props.precision))
+const values = computed(() => {
+  const [minValue, maxValue] = state.value.start;
+  const { min: minRange, max: maxRange } = state.value.range;
+  return {
+    min: minValue !== -Infinity && minValue !== minRange ? minValue : minRange,
+    max: maxValue !== Infinity && maxValue !== maxRange ? maxValue : maxRange,
+  };
+});
 
-  const values = computed(() => {
-      const [minValue, maxValue] = state.value.start
-      const { min: minRange, max: maxRange } = state.value.range;
-      return {
-        min:
-          minValue !== -Infinity && minValue !== minRange
-            ? minValue
-            : minRange,
-        max:
-          maxValue !== Infinity && maxValue !== maxRange ? maxValue : maxRange,
-      };
-  })
-
-  const pick = (first: number | null | undefined , second: number)  => {
-      if (first !== null && first !== undefined) {
-          return first;
-      } else {
-          return second;
-      }
+const pick = (first: number | null | undefined, second: number) => {
+  if (first !== null && first !== undefined) {
+    return first;
+  } else {
+    return second;
   }
+};
 
-  const refine = (data: { min: number, max: number }) => {
-    state.value.refine([data.min, data.max]);
-  }
+const refine = (data: { min: number; max: number }) => {
+  state.value.refine([data.min, data.max]);
+};
 </script>
 <template>
   <div class="range-input">
@@ -71,7 +63,12 @@ import type { range } from "instantsearch.js/es/lib/utils";
       >
         <form
           :class="suit('form')"
-          @submit.prevent="refine({ min: pick(minInput, state.range.min), max: pick(maxInput, state.range.max) })"
+          @submit.prevent="
+            refine({
+              min: pick(minInput, state.range.min),
+              max: pick(maxInput, state.range.max),
+            })
+          "
         >
           <label :class="suit('label')">
             <slot name="minLabel" />
@@ -81,10 +78,15 @@ import type { range } from "instantsearch.js/es/lib/utils";
               :step="step"
               :min="state.range.min"
               :max="state.range.max"
-              :placeholder="state.range.min"
+              :placeholder="state.range.min.toString()"
               :value="values.min"
-              @change="($event) => minInput = parseFloat(($event?.currentTarget as HTMLInputElement)?.value)"
-            >
+              @change="
+                ($event) =>
+                  (minInput = parseFloat(
+                    ($event?.currentTarget as HTMLInputElement)?.value,
+                  ))
+              "
+            />
           </label>
           <span :class="suit('separator')">
             <slot name="separator">to</slot>
@@ -97,18 +99,18 @@ import type { range } from "instantsearch.js/es/lib/utils";
               :step="step"
               :min="state.range.min"
               :max="state.range.max"
-              :placeholder="state.range.min"
+              :placeholder="state.range.min.toString()"
               :value="values.max"
-              @change="($event) => maxInput = parseFloat(($event?.currentTarget as HTMLInputElement)?.value)"
-            >
+              @change="
+                ($event) =>
+                  (maxInput = parseFloat(
+                    ($event?.currentTarget as HTMLInputElement)?.value,
+                  ))
+              "
+            />
           </label>
-          <button
-            :class="suit('submit')"
-            type="submit"
-          >
-            <slot name="submitLabel">
-              Go
-            </slot>
+          <button :class="suit('submit')" type="submit">
+            <slot name="submitLabel"> Go </slot>
           </button>
         </form>
       </slot>
